4783ec7f5770b622ccb6d5e0046aaf2b65d54b18
JayKean <62370359+JayKean@users.noreply.github.com>
2022-05-12 00:00:15 +1200

    added upto date comments

--- Palindrome.cs
+++ Palindrome.cs
@@ -7,21 +7,26 @@ public class Palindrome
 {
     static void Main(string[] args)
     {
-        /*creates string variables to store 2 instances of the users word
-        to be compared after input */
+        /*creates string variables to store 2 instances of the users input
+        to be compared later*/
         string UserWord, RevWord = string.Empty;
     
-        //prompts the user to input
+        //prompts the user to input and stores it in a string variable
         Console.Write("Enter Word: ");
-        //stores user input in a string variable
         UserWord = Console.ReadLine();
         
+        //only runs palindrome checker if user has inputted accepted value
         if (UserWord != null)
         {
+            //simple loop that reverses the inputted word and stores to revWord variable
             for (int i = UserWord.Length - 1; i >=0; i--)
             {
                 RevWord += UserWord[i].ToString();
             }
+
+            /*compares the reversed word to the original word
+            if they are equal then the word is a palindrome 
+            returns a response to the user*/
             if (RevWord == UserWord)
             {
                 Console.WriteLine("The Word {0} is a Palindrome!!!", UserWord);
